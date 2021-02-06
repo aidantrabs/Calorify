cd67e18c291232f46595eb5039b084a197d8b268
VishvamPorwal <59139142+VishvamPorwal@users.noreply.github.com>
2021-02-07 04:46:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-#Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+from nutritionix import Nutritionix
 
-#Iniitalize Flask app
+nix = Nutritionix(app_id="c1955429",
+                  api_key="4e8e629653b6d84fda5ad79b71b805f2")
+
+
+
 app = Flask(__name__)
 
-#Front page
-@app.route('/')
-def indexPage():
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        return render_template("foods.html")
+    if request.method == 'POST':
+        food = request.form.get("foods")
+        results = nix.search(food, results="0:1").json()
+        value = results['hits']
+        temp = value[0]
+        info = nix.item(id = temp['_id']).json()
+        print(info)
+        return render_template("info.html", info = info)
 
-    return
 
 #Main method
 if __name__ == '__main__':
