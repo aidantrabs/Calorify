487481cf721b9ccbb4534e04b45f865a539ab708
aidantrabs <aidantraboulay13@gmail.com>
2021-02-06 19:27:34 -0500

    added .gitignore

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from flask import Flask, render_template, request
 import json
 from nutritionix import Nutritionix
+from keys import keys_index
 
-nix = Nutritionix(app_id="c1955429",
-                  api_key="4e8e629653b6d84fda5ad79b71b805f2")
-
-
+nix = Nutritionix(app_id=keys_index[0],
+                  api_key=keys_index[1])
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
+
     if request.method == 'GET':
+
         return render_template("foods.html")
+
     if request.method == 'POST':
+
         food = request.form.get("foods")
         results = nix.search(food, results="0:1").json()
         value = results['hits']
         temp = value[0]
         info = nix.item(id = temp['_id']).json()
+        
         print(info)
+
         return render_template("info.html", info = info)
 
 
